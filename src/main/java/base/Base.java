71b024bb3ecc69f3f9ff38091a7bba1d8b25dbfa
JAY RAJ <jayraj@Jays-MacBook-Air.local>
2026-03-12 16:30:17 +0530

    Update in Base

--- src/main/java/base/Base.java
+++ src/main/java/base/Base.java
@@ -1,5 +1,16 @@
 package base;
 
-public class Base {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
 
+public class Base {
+public WebDriver driver;
+
+
+ public Base (WebDriver driver){
+	this.driver=driver;
+	PageFactory.initElements(driver,this);
 }
+	
+}
+
