b1a6956dc8da346a98bb58c2e2f9d08c57d3266a
JAY RAJ <jayraj@Jays-MacBook-Air.local>
2026-03-12 16:17:03 +0530

    Add BaseTest

---
+++ src/test/java/BaseTest/BaseTests.java
@@ -0,0 +1,26 @@
+package BaseTest;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class BaseTests {
+	public WebDriver driver;
+	@BeforeMethod
+	public void setup()
+	{
+		driver=new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get("https://www.myntra.com/");
+		driver.manage().window().maximize();
+	}
+	@AfterMethod
+	public void close()
+	{
+		driver.quit();
+	}
+	
+}
